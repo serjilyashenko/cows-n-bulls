50c82a3ce3a6da855b01e17ad2db259acd11568b
Serge Ilyashenko <age_bookend_0j@icloud.com>
2022-08-25 19:56:32 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <main className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Cows & Bulls</h1>
+    </main>
+  )
+}
