d7571d5047bea8c19fb6c5fa368a3112c0d1132e
Serge Ilyashenko <age_bookend_0j@icloud.com>
2022-08-30 16:04:06 +0200

    seed word error

--- src/utils/seed-words.ts
+++ src/utils/seed-words.ts
@@ -1,9 +1,11 @@
 const SEED_WORDS_VALUE: unknown = import.meta.env.VITE_SEED_WORDS;
 
 if (!(typeof SEED_WORDS_VALUE === "string")) {
+  alert("Sorry. Something went wrong");
   throw new Error("There is no VITE_SEED_WORDS");
 }
 if (SEED_WORDS_VALUE.split(" ").length < 72) {
+  alert("Sorry. Something went wrong");
   throw new Error("There are not enough seed words");
 }
 
